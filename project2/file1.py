c192b3ea1453e8a763e7101eb780ec90b56f57f2
abhishekla311 <abhishekla311@gmail.com>
2026-07-10 09:28:01 -0700

    add data

---
+++ project2/file1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+data = {
+    'Name':['Alic', 'Customer', 'Bob', ],
+    'age':[23,34,24]
+}
+
+
+df = pd.DataFrame(data)
+
+# new_roc = {'Name':'GIFE', 'age':30}
+# df.loc[len(df.index)] = new_roc
+
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok =True)
+
+file_path = os.path.join(data_dir, 'data.csv')
+
+df.to_csv(file_path, index=False)
+
+print(f"dataframe {file_path}")
